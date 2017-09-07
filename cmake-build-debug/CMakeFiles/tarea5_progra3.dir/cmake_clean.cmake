e3c48e093ac530c766f1f6e8771da7cc4ac7c3c6
DavidNu <davidedgardonu121@gmail.com>
2017-09-06 21:47:46 -0600

    Tarea

---
+++ cmake-build-debug/CMakeFiles/tarea5_progra3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tarea5_progra3.dir/main.cpp.obj"
+  "CMakeFiles/tarea5_progra3.dir/Evaluador.cpp.obj"
+  "tarea5_progra3.pdb"
+  "tarea5_progra3.exe"
+  "tarea5_progra3.exe.manifest"
+  "libtarea5_progra3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tarea5_progra3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
